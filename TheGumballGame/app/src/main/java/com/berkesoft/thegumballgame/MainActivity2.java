921c2a3d82d64330b3e53207957fed10563c4f1f
Berkeerkec <101473351+Berkeerkec@users.noreply.github.com>
2022-08-24 19:05:47 +0300

    The Gumball Game
    Belirli zaman içerisinde karakterleri yakala ve skorları topla.

---
+++ TheGumballGame/app/src/main/java/com/berkesoft/thegumballgame/MainActivity2.java
@@ -0,0 +1,29 @@
+package com.berkesoft.thegumballgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+    public void start(View view){
+        Intent intent = new Intent(MainActivity2.this,MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void exit(View view){
+        Intent setIntent = new Intent(Intent.ACTION_MAIN);
+        setIntent.addCategory(Intent.CATEGORY_HOME);
+        setIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(setIntent);
+
+    }
+}
